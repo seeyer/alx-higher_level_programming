935a52bf2440590a7adce3d44e6260ee4b10652e
seeyer <45285003+seeyer@users.noreply.github.com>
2022-09-07 01:10:50 +0100

    JavaScript - Warm up project1

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Define a function to call a function x times
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i += 1) {
+    theFunction();
+  }
+};
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Pri
